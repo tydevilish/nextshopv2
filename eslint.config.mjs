016f652abc5cb7753dc61c066046dad1b12323b6
korawit <gmil1246ftg@gmail.com>
2025-07-02 08:28:21 +0700

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["app/generated/prisma/**"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
